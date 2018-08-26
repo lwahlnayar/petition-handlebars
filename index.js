08f4e2c1acdef1455343330591a7281e29d62b4e
Rodrigo Wahl <lwahlnayar@gmail.com>
2018-08-26 22:47:04 +0200

    fix and test

--- index.js
+++ index.js
@@ -128,8 +128,6 @@ app.post("/profile", (req, res) => {
 
 app.get("/profile/edit", checkForUserSession, (req, res) => {
     getUserEditData(req.session.loggedIn).then(userData => {
-        console.log("LOGGED IN SESSION COOKIE-->", req.session.loggedIn);
-        console.log("USER DATA from GETUSEREDITDATA --->", userData);
         res.render("edit.handlebars", {
             layout: "secondary_layout.handlebars",
             userData: userData
